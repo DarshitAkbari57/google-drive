c1fc0bd2277b78b906914d2553d4e0d4f2f551b2
Darshit Akbari <darshitakbari5799@gmail.com>
2025-05-02 17:04:43 +0530

    create uploads folder in root dir

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 // Multer setup for file uploads
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname, 'uploads');
+        const uploadPath = path.join(process.cwd(), 'uploads');
 
         // Create the uploads directory if it doesn't exist
         if (!fs.existsSync(uploadPath)) {
